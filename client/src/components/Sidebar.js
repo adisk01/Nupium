4cbab41a90484a61c447ef64140dfe38acdd5065
adisk01 <adiscrazy01@gmail.com>
2023-12-25 15:07:09 +0530

    Logout Done

--- client/src/components/Sidebar.js
+++ client/src/components/Sidebar.js
@@ -1,8 +1,11 @@
 import React, { useState } from 'react';
-import './Sidebar.css'
+import './Sidebar.css';
+import { Link, useNavigate } from "react-router-dom";
+
 import {
   FaTh,
   FaBars,
+  FaSignOutAlt,
   FaUserAlt,
   FaRegChartBar,
   FaCommentAlt,
@@ -11,11 +14,17 @@ import {
 } from "react-icons/fa";
 import { NavLink } from 'react-router-dom';
 
-import './Sidebar.css';
-
 const Sidebar = ({ children }) => {
   const [isOpen, setIsOpen] = useState(false);
+  const navigate = useNavigate(); // Move useNavigate here
   const toggle = () => setIsOpen(!isOpen);
+
+  const handleLogout = () => {
+    // Implement your logout logic here
+    console.log('Logout button clicked');
+    localStorage.removeItem('token');
+    navigate('/login');
+  };
 
   const menuItem = [
     {
@@ -65,34 +74,37 @@ const Sidebar = ({ children }) => {
       path: '/help',
       icon: <FaTh />,
       cName: 'nav-text'
-    }
+    },
+    {
+      title: 'Logout',
+      icon: <FaSignOutAlt />,
+      cName: 'nav-text',
+      onClick: handleLogout
+    },
   ];
 
   return (
-    <div className={`container ${isOpen ? 'open' : 'closed'}`}>
-      <div className="sidebar">
-        <div className="top_section">
-          <h1 className={`logo ${isOpen ? 'visible' : 'hidden'}`}>Logo</h1>
-          <div className="bars">
-            <FaBars onClick={toggle} />
-          </div>
-        </div>
-        {menuItem.map((item, index) => (
-          <NavLink
-            to={item.path}
-            key={index}
-            className="link"
-            activeClassName="active" // This class will be applied when the link is active
-          >
-            <div className="icon">{item.icon}</div>
-            <div className={`link_text ${isOpen ? 'visible' : 'hidden'}`}>
-              {item.title}
+    <>
+      <div className={`container ${isOpen ? 'open' : 'closed'}`}>
+        <div className="sidebar">
+          <div className="top_section">
+            <h1 className={`logo ${isOpen ? 'visible' : 'hidden'}`}>Logo</h1>
+            <div className="bars">
+              <FaBars onClick={toggle} />
             </div>
-          </NavLink>
-        ))}
+          </div>
+          {menuItem.map((item, index) => (
+            <div key={index} className="link" onClick={item.onClick}>
+              <div className="icon">{item.icon}</div>
+              <div className={`link_text ${isOpen ? 'visible' : 'hidden'}`}>
+                {item.title}
+              </div>
+            </div>
+          ))}
+        </div>
+        <main>{children}</main>
       </div>
-      <main>{children}</main>
-    </div>
+    </>
   );
 };
 
